1465fa5096e220ca91fa86284e1de9bf292fefdb
Mewadaajay902 <68866722+Mewadaajay902@users.noreply.github.com>
2025-04-07 04:00:21 -0700

    Update provider.tf for backend setup for statefiles

--- provider.tf
+++ provider.tf
@@ -6,13 +6,12 @@ terraform {
     }
   }
   required_version = ">= 1.3.0"
-  # backend "azurerm" {
-  #   sas_token = "sp=racwdli&st=2025-04-01T09:23:37Z&se=2025-04-01T17:23:37Z&spr=https&sv=2024-11-04&sr=c&sig=1zlQhvC14lsJhvW7S5MX7%2FEiAEbh0aYUk9Sf5nuCgRA%3D"
-  #   storage_account_name = "storageforterraformgvsx"
-  #   container_name = "state-data"
-  #   key = "prodtest.terraform.tfstate"
+   backend "azurerm" {
+     storage_account_name = "storageforterraformgvsx"
+     container_name = "state-data"
+     key = "prodtest.terraform.tfstate"
 
-  # }
+   }
 }
 
 provider "azurerm" {
